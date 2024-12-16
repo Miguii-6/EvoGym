2c8bcf01d9ebec37730a9c147bc5c5e59ea26cd2
Miguel Mariño Martínez <114078229+Miguii-6@users.noreply.github.com>
2024-12-16 12:15:32 +0100

    Delete carpeta no necesaria

--- xd/EvoGym/EvoGym.Client/Services/ISessionStorageService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EvoGym.Client.Services
-{
-    public interface ISessionStorageService
-    {
-        string GetItem(string key);
-        void SetItem(string key, string value);
-        void RemoveItem(string key);
-    }
-}
